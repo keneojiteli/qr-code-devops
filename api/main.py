9cfef490020c656cce8a14bc2eacb15e0420b0aa
keneojiteli <ojitelikenechukwu@gmail.com>
2025-04-24 12:27:15 +0100

    add project files for devops-qr-code project

--- api/main.py
+++ api/main.py
@@ -1,39 +1,41 @@
 from fastapi import FastAPI, HTTPException
-from fastapi.middleware.cors import CORSMiddleware
-import qrcode
-import boto3
-import os
-from io import BytesIO
+from fastapi.middleware.cors import CORSMiddleware #allows fe to connect to be even on diff domains
+import qrcode #library to generate QR codes
+import boto3 #aws sdk for python
+import os #to access environment variables (like AWS keys) and work with file paths
+from io import BytesIO #holds image data in memory, like a temporary file
 
-# Loading Environment variable (AWS Access Key and Secret Key)
-from dotenv import load_dotenv
+# Loading Environment variable (AWS Access Key and Secret Key) from a .env file
+from dotenv import load_dotenv 
 load_dotenv()
 
-app = FastAPI()
+app = FastAPI() #create a FastAPI instance
 
 # Allowing CORS for local testing
 origins = [
-    "http://localhost:3000"
+    # "http://localhost:3000",
+    "http://127.0.0.1:3000"
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    # allow_origins=origins,
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# AWS S3 Configuration
+# AWS S3 Configuration, connects to s3 with config from .env
 s3 = boto3.client(
     's3',
     aws_access_key_id= os.getenv("AWS_ACCESS_KEY"),
     aws_secret_access_key= os.getenv("AWS_SECRET_KEY"))
 
-bucket_name = 'YOUR_BUCKET_NAME' # Add your bucket name here
+bucket_name = 'qr-code-bucket-17-04-25' # Add your bucket name here
 
 @app.post("/generate-qr/")
 async def generate_qr(url: str):
-    # Generate QR Code
+    # Generate QR Code and setup its config
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
@@ -55,7 +57,8 @@ async def generate_qr(url: str):
 
     try:
         # Upload to S3
-        s3.put_object(Bucket=bucket_name, Key=file_name, Body=img_byte_arr, ContentType='image/png', ACL='public-read')
+        # s3.put_object(Bucket=bucket_name, Key=file_name, Body=img_byte_arr, ContentType='image/png', ACL='public-read')
+        s3.put_object(Bucket=bucket_name, Key=file_name, Body=img_byte_arr, ContentType='image/png')
         
         # Generate the S3 URL
         s3_url = f"https://{bucket_name}.s3.amazonaws.com/{file_name}"
--- front-end-nextjs/src/app/page.js
+++ front-end-nextjs/src/app/page.js
@@ -10,7 +10,8 @@ export default function Home() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post(`http://localhost:8000/generate-qr/?url=${url}`);
+      // const response = await axios.post(`http://localhost:8000/generate-qr/?url=${url}`);
+      const response = await axios.post(`http://127.0.0.1:8000/generate-qr/?url=${url}`);
       setQrCodeUrl(response.data.qr_code_url);
     } catch (error) {
       console.error('Error generating QR Code:', error);
